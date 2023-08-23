35f5aa4ce40d10b0e7f27d553b38fa5c62f5f3ff
Cauê Kotarski <caue1017@hotmail.com>
2023-08-23 17:38:29 -0300

    feat: create selected item color in header nav

--- src/layouts/DefaultLayouts/index.tsx
+++ src/layouts/DefaultLayouts/index.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../Components/Header'
+import { Header } from '../../Components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
     <>
-      <div>
+      <LayoutContainer>
         <Header />
         <Outlet />
-      </div>
+      </LayoutContainer>
     </>
   )
 }
